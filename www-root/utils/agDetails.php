d19040124cb189cb95219acbd655e4c780d3c888
Sobottasgithub <patrick.schulze.ofp@gmail.com>
2026-05-10 18:49:42 +0200

    update: add detail page

---
+++ www-root/utils/agDetails.php
@@ -0,0 +1,8 @@
+<?php
+if(isset($_POST['agName'])){
+    echo "hello!";
+    echo "->" . $_POST['agName']. " !";
+} else {
+    echo "no data!";
+}
+?>
--- www-root/utils/agDisplay.php
+++ www-root/utils/agDisplay.php
@@ -1,15 +1,16 @@
 <?php
     require __DIR__ . "/../login/defaultUser.php";
 
-    $query = "SELECT Vorname, Nachname, Leitung, Raum, Wochentag FROM Ag JOIN Lehrer ON Ag.Leitung = Lehrer.Kuerzel";
+    $query = "SELECT Name, Vorname, Nachname, Leitung, Raum, Wochentag FROM Ag JOIN Lehrer ON Ag.Leitung = Lehrer.Kuerzel";
     $result = $conn->query($query);
 
     echo "<table>";
     echo "<tr><td>Vorname</td><td>Nachname</td><td>Leitung</td><td>Raum</td><td>Wochentag</td></tr>";
-
+    echo "<form action='/utils/agDetails.php' method='post'>";
     if ($result->rowCount() > 0) {
         while($row = $result->fetch(PDO::FETCH_ASSOC)) {
             echo "<tr>";
+            echo "<td><button type='submit' name='agName' value='".$row["Name"]."'>" . $row["Name"] . "</button></td>";
             echo "<td>" . $row["Vorname"] . "</td>";
             echo "<td>" . $row["Nachname"] . "</td>";
             echo "<td>" . $row["Leitung"] . "</td>";
@@ -18,6 +19,7 @@
             echo "</tr>";
         }
     }
+    echo "</form>";
     echo "</table>";
 
     $conn = null;
