7bc6201110c4d5b765793a1a99553bf04cd4b25a
Sobottasgithub <patrick.schulze.ofp@gmail.com>
2026-05-27 15:21:11 +0200

    update: fix auto accept ag

--- www-root/utils/autoAcceptAg.php
+++ www-root/utils/autoAcceptAg.php
@@ -6,13 +6,13 @@
 
     if ($agDataResult->rowCount() > 0) {
         while($agDataRow = $agDataResult->fetch(PDO::FETCH_ASSOC)) {            
-            $teilnehmerCountQuery = "SELECT COUNT(*) AS count FROM Teilnahme WHERE AgName='" . $agDataRow["Name"] . "' AND Genehmigt=1";
-            $teilnehmerCountResult = $conn->query($teilnehmerCountQuery);
-            $count = $teilnehmerCountResult->fetch(PDO::FETCH_ASSOC);
-
+            $teilnehmerCountStatement = $conn->prepare("SELECT COUNT(*) AS count FROM Teilnahme WHERE AgName = :name AND Genehmigt=1");
+            $teilnehmerCountStatement->execute([':name' => $agDataRow["Name"]]);
+            $count = $teilnehmerCountStatement->fetch(PDO::FETCH_ASSOC);
+            
             if ($count["count"] >= 10) {
-                $query = "UPDATE Ag SET FindetStatt=true WHERE Name='" . $_POST['submitAgGenehmigung'] . "'";
-                $result = $conn->query($query);
+                $setGenehmigtStatement = $conn->prepare("UPDATE Ag SET FindetStatt=true WHERE Name = :name");
+                $setGenehmigtStatement->execute([':name' => $agDataRow["Name"]]);
             }
         }
     }
