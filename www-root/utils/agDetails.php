e3a416a278000ac60781a7e1fbf8037e765863b0
Patrick Schulze <patrick.schulze.ofp@gmail.com>
2026-05-28 10:04:34 +0200

    update: switch details to pdo statements

--- www-root/utils/agDetails.php
+++ www-root/utils/agDetails.php
@@ -18,30 +18,31 @@
                             if(isset($_POST['agName'])){
                                 require __DIR__ . "/../login/defaultUser.php";
 
-                                $query = "SELECT Name, Vorname, Nachname, Leitung, Raum, Wochentag, Beschreibung
-                                         FROM Ag JOIN Lehrer ON Ag.Leitung = Lehrer.Kuerzel WHERE Name='".$_POST['agName']."'";
-                                $result = $conn->query($query);
+                                $agName = $_POST['agName'];
+                                $getAgStatement = $conn->prepare("SELECT Name, Vorname, Nachname, Leitung, Raum, Wochentag, Beschreibung
+                                         FROM Ag JOIN Lehrer ON Ag.Leitung = Lehrer.Kuerzel WHERE Name = :name");
+                                $getAgStatement->execute([':name' => $agName]);
 
-                                $teilnehmerCountQuery = "SELECT COUNT(*) AS count FROM Teilnahme WHERE AgName='" . $_POST['agName']. "' AND Genehmigt=1";
-                                $teilnehmerCountResult = $conn->query($teilnehmerCountQuery);
-                                $count = $teilnehmerCountResult->fetch(PDO::FETCH_ASSOC); 
-            
-                                if ($result->rowCount() == 1) {
-                                    $row = $result->fetch(PDO::FETCH_ASSOC);
-                                    echo "<h1>" . $row["Name"] . "</h1>";
-                                    echo "<table>";
-                                    echo "<tr><td>Vorname</td><td>Nachname</td><td>Leitung</td><td>Raum</td><td>Wochentag</td><td>Teilnehmer</td></tr>";
-                                    echo "<tr>";
-                                    echo "<td>" . $row["Vorname"] . "</td>";
-                                    echo "<td>" . $row["Nachname"] . "</td>";
-                                    echo "<td>" . $row["Leitung"] . "</td>";
-                                    echo "<td>" . $row["Raum"] . "</td>";
-                                    echo "<td>" . $row["Wochentag"] . "</td>";
-                                    echo "<td>" . $count["count"] . "</td>";
-                                    echo "</tr>";
-                                    echo "</table><br>";
-                                    echo "<p>" . $row["Beschreibung"] . "</p>";
-                                }
+                                $teilnehmerCountStatement = $conn->prepare("SELECT COUNT(*) AS count FROM Teilnahme WHERE AgName = :name AND Genehmigt=1");
+                                $teilnehmerCountStatement->execute([':name' => $agName]);
+                                
+                                $count = $teilnehmerCountStatement->fetch(PDO::FETCH_ASSOC); 
+
+                                $row = $getAgStatement->fetch(PDO::FETCH_ASSOC);
+                                echo "<h1>" . $row["Name"] . "</h1>";
+                                echo "<table>";
+                                echo "<tr><td>Vorname</td><td>Nachname</td><td>Leitung</td><td>Raum</td><td>Wochentag</td><td>Teilnehmer</td></tr>";
+                                echo "<tr>";
+                                echo "<td>" . $row["Vorname"] . "</td>";
+                                echo "<td>" . $row["Nachname"] . "</td>";
+                                echo "<td>" . $row["Leitung"] . "</td>";
+                                echo "<td>" . $row["Raum"] . "</td>";
+                                echo "<td>" . $row["Wochentag"] . "</td>";
+                                echo "<td>" . $count["count"] . "</td>";
+                                echo "</tr>";
+                                echo "</table><br>";
+                                echo "<p>" . $row["Beschreibung"] . "</p>";
+                                
                                 $conn = null;
                             } else {
                                 echo "Something went wrong!";
